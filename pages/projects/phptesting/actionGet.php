ac8699ded8e7454a741bc3497444c48721ec8a7d
Josh <joshhKing97@gmail.com>
2017-08-23 12:10:03 +0100

    fix

--- pages/projects/phptesting/actionGet.php
+++ pages/projects/phptesting/actionGet.php
@@ -1,4 +1,8 @@
-<?php
-    echo "Hi ".$_GET['name'].".";
-    echo "You are ".$_GET['age']." years old.";
-?>
+<html>
+    <body>
+        <?php
+            echo "Hi ".$_GET['name'].".";
+            echo "You are ".$_GET['age']." years old.";
+        ?>
+    </body>
+</html>
